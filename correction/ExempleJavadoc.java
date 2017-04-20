b859eb4c6ee1556f45f47381d7bcebea34e305fd
Laurent HUBERT <lau.hub@gmail.com>
2017-04-20 15:41:38 +0200

    Ajout exemple Javadoc

---
+++ correction/ExempleJavadoc.java
@@ -0,0 +1,22 @@
+//ExempleJavadoc.java
+
+/**
+ * Ma classe d'exemple
+ * @author lauhub
+ *
+ */
+public class ExempleJavadoc {
+	/** Valeur de l'exemple */ // Javadoc
+	public int valeur ;
+
+	/** La chaîne de l'exemple */
+	public String chaine ;
+
+	/**
+	 * Cette méthode renvoie la chaine de l'exemple
+	 * @return une chaine de caractère (String)
+	 */
+	public String getChaine(){
+		return chaine;
+	}
+}
